5f697b24ded5f587bc4ce1348d145bc2ef9a0230
jaggehns <jaggehns@gmail.com>
2022-03-13 04:33:51 +0800

    useQuery

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,22 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchedSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data } = useQuery("super-heroes", fetchedSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </>
+  );
+};
