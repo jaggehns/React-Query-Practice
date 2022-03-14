f77234b12323f11a4d6e7748d98ca09fe4da56fd
jaggehns <jaggehns@gmail.com>
2022-03-15 03:49:32 +0800

    Query Id

---
+++ react-query-starter/src/components/RQSuperHero.page.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSuperHeroData } from "../hooks/useSuperHeroData";
+
+const RQSuperHeroPage = () => {
+  const { heroId } = useParams();
+  const { isLoading, data, isError, error } = useSuperHeroData(heroId);
+
+  if (isLoading) return <h2>Loading...</h2>;
+
+  if (isError) return <h2>{error.message}</h2>;
+  return (
+    <>
+      {data?.data.name} - {data?.data.alterEgo}
+    </>
+  );
+};
+
+export default RQSuperHeroPage;
--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
@@ -27,12 +28,14 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
-      {/* {data?.data.map((hero) => (
-        <div key={hero.name}>{hero.name}</div>
-      ))} */}
-      {data.map((heroName) => {
+      {data?.data.map((hero) => (
+        <div key={hero.id}>
+          <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+        </div>
+      ))}
+      {/* {data.map((heroName) => {
         return <div key={heroName}>{heroName}</div>;
-      })}
+      })} */}
     </>
   );
 };
